81b38240849d25303b3630689ed7d3a5fe68a745
mr-b4rc0de <168055883+mr-b4rc0de@users.noreply.github.com>
2024-06-30 22:15:15 -0400

    Update upload.py

--- upload.py
+++ upload.py
@@ -249,8 +249,8 @@ async def do_the_thing(base_dir):
             else:
                 nfo_path = os.path.splitext(path)[0] + '.nfo'
             if os.path.exists(nfo_path):
-                if not os.path.exists(f"{base_dir}/tmp/{os.path.basename(path)}/{os.path.basename(nfo_path)}"):
-                    shutil.copyfile(nfo_path,f"{base_dir}/tmp/{os.path.basename(path)}/{os.path.basename(nfo_path)}")            
+                if not os.path.exists(f"{base_dir}/tmp/{os.path.basename(path)}/{meta['clean_name']}.nfo"):
+                    shutil.copyfile(nfo_path,f"{base_dir}/tmp/{os.path.basename(path)}/{meta['clean_name']}.nfo")            
 
         ####################################
         #######  Upload to Trackers  #######
